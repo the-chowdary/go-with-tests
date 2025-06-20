ba40d6d9907948a1ab50f2c31d23122596896172
Aneesh Rayala <aneeshr0302@gmail.com>
2025-06-20 09:29:47 -0500

    chore(01): removes commented out code

--- 01-hello/hello.go
+++ 01-hello/hello.go
@@ -13,14 +13,6 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "world!"
 	}
-	// if language == "" || strings.ToLower(language) == "english" {
-	// 	language = englishHelloPrefix
-	// } else if strings.ToLower(language) == "spanish" {
-	// 	language = spanishHelloPrefix
-	// } else {
-	// 	language = frenchHelloPrefix
-	// }
-	// return language + name
 
 	prefix := englishHelloPrefix
 
