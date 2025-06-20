36ca5c8973774d17f02b67be289aa16ebc4af744
Aneesh Rayala <aneeshr0302@gmail.com>
2025-06-19 21:13:02 -0500

    feat(01): changes if else conditions to switch case

--- 01-hello/hello.go
+++ 01-hello/hello.go
@@ -13,14 +13,24 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "world!"
 	}
-	if language == "" || strings.ToLower(language) == "english" {
-		language = englishHelloPrefix
-	} else if strings.ToLower(language) == "spanish" {
-		language = spanishHelloPrefix
-	} else {
-		language = frenchHelloPrefix
+	// if language == "" || strings.ToLower(language) == "english" {
+	// 	language = englishHelloPrefix
+	// } else if strings.ToLower(language) == "spanish" {
+	// 	language = spanishHelloPrefix
+	// } else {
+	// 	language = frenchHelloPrefix
+	// }
+	// return language + name
+
+	prefix := englishHelloPrefix
+
+	switch strings.ToLower(language) {
+	case "spanish":
+		prefix = spanishHelloPrefix
+	case "french":
+		prefix = frenchHelloPrefix
 	}
-	return language + name
+	return prefix + name
 }
 
 func main() {
